13305bbef457de3fe5a5fa20a5e2855e7524ed4f
Mustapha Tijani <thenewxpat@gmail.com>
2026-07-08 11:01:03 +0100

    feat: expand and update department subject mappings in DepartmentSubjectSeeder

--- database/seeders/DepartmentSubjectSeeder.php
+++ database/seeders/DepartmentSubjectSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Department;
 use App\Models\Subject;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DepartmentSubjectSeeder extends Seeder
@@ -15,9 +14,48 @@ class DepartmentSubjectSeeder extends Seeder
     public function run(): void
     {
         $departments = [
-            'Science' => ['Mathematics', 'English', 'Biology', 'Chemistry', 'Physics', 'Geography'],
-            'Art' => ['Mathematics', 'English', 'Literature', 'Government', 'History'],
-            'Commercial' => ['Mathematics', 'English', 'Economics', 'Geography', 'Government'],
+            'Science' => [
+                'Mathematics',
+                'English Language',
+                'Biology',
+                'Chemistry',
+                'Physics',
+                'Geography',
+                'Agricultural Science',
+                'Further Mathematics',
+                'Health Science',
+                'Computer Studies/ICT',
+                'Technical Drawing',
+                'Civic Education',
+            ],
+
+            'Art' => [
+                'Mathematics',
+                'English Language',
+                'Literature-in-English',
+                'Government',
+                'History',
+                'Christian Religious Studies (CRS) / Islamic Religious Studies (IRS)',
+                'Fine Arts',
+                'Music',
+                'French',
+                'Nigerian Language (Yoruba/Igbo/Hausa)',
+                'Geography',
+                'Civic Education',
+            ],
+
+            'Commercial' => [
+                'Mathematics',
+                'English Language',
+                'Economics',
+                'Financial Accounting',
+                'Commerce',
+                'Government',
+                'Geography',
+                'Business Studies',
+                'Insurance',
+                'Civic Education',
+            ],
         ];
 
         foreach ($departments as $deptName => $subjectNames) {
